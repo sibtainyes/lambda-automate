95d883da8f7abb9d8dcc993fa630ca26a1216c83
sibtainyes <sibtain.abbas10@gmail.com>
2017-08-02 11:09:32 +0500

    - trying to fix AccessDenied error 3

--- rest_um_signin.py
+++ rest_um_signin.py
@@ -23,7 +23,7 @@ __date__        = 25, April 2017
 __version__     = 1.0.0
 """
 
-# This comment is to test automation 2
+# This comment is to test automation 3
 
 def lambda_handler(event, context):
     """
